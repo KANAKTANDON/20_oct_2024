9ce7f453ea6c039b1edc409704d40b09934414f4
KANAKTANDON <knktndn@gmail.com>
2024-10-21 13:07:02 +0530

    first commit for jenkins

---
+++ src/main/java/_oct_2024_jenkins_learning/_oct_2024_jenkins_learning/App.java
@@ -0,0 +1,13 @@
+package _oct_2024_jenkins_learning._oct_2024_jenkins_learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
